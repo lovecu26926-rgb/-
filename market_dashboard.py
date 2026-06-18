b32711d7fffc717721bdaee2ade2818b29451633
lovecu26926-rgb <lovecu26926@gmail.com>
2026-06-19 04:21:22 +0900

    Update market_dashboard.py

--- market_dashboard.py
+++ market_dashboard.py
@@ -10,7 +10,7 @@ import pytz
 # 🔐 텔레그램 설정 (Secrets에서 가져옴)
 # =========================
 TELEGRAM_TOKEN = os.getenv("8680217169")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_CHAT_ID = os.getenv("61473298612")
 
 def send_telegram(message):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
